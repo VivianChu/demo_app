db6d436904b79013f7cf83848f20d08ab9d2e630
VivianChu <tchu@ucsd.edu>
2015-05-27 14:41:22 -0700

    Testing only

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 DemoApp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  #
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
